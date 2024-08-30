94a96e9c98e7d98f7d2fcc59acc2dc0c74101af6
Jose <Udermy_right@hotmail.com>
2024-08-30 00:21:44 -0300

    adicionando aula 3 completa

--- Python/aula3.py
+++ Python/aula3.py
@@ -1,16 +1,23 @@
 """
-
+DocString
 Python = Linguagem de programação
 Tipo de tipagem = Dinâmica / Forte
 str -> string -> texto
 Strings são textos que estão dentro de aspas
 
 """
-
+print(1234, end="\r\n\n\n")
 # Aspas simples
+print('Luiz Otávio',)
+print('Luiz "Otávio"')
+print(1, 'Luiz "Otávio"', end="\r\n\n\n")
 
 # Aspas duplas
+print("Luiz Otávio")
+print(2, "Luiz f'Otávio'", end="\r\n\n\n")
 
 # Escape
+print("Luiz \"Otávio\"", end="\r\n\n\n")
 
 # r
+print(r"Luiz \"Otávio\"", end="\r\n\n\n")
